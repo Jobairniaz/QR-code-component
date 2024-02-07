25225a3c948b41de6dfeca863d285b01100ce33b
Jobair Niaz <jbmniaz0@gamil.com>
2024-02-08 03:27:10 +0600

    some

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/QR-code-component/",
+  base: "/qr-code-project/",
   plugins: [react()],
 });
